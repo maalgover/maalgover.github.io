f1c1a01410946e5361c427cb61c30a22af8d74a3
maalgover <maalgover@gmail.com>
2021-03-01 00:32:44 +0100

    tweaking the fetch parameters

--- projects/inegi_api/inegi_api.js
+++ projects/inegi_api/inegi_api.js
@@ -14,14 +14,13 @@
 console.log("This is just a heart beat");
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('getMessage').onclick = function(){
       const req = new XMLHttpRequest();
       req.open("GET","https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json",true);
       req.send();
       req.onload = function(){
         const json = JSON.parse(req.responseText);
         console.log(json)
-    }}})
+    }})
 
 
 //fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json")
