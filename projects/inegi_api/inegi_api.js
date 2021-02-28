90f06adc1db7ba3ee90b6dbc3689e64e93b50ae9
maalgover <maalgover@gmail.com>
2021-03-01 00:45:36 +0100

    tweaking the fetch parameters

--- projects/inegi_api/inegi_api.js
+++ projects/inegi_api/inegi_api.js
@@ -1,9 +1,5 @@
 console.log("This is just a heart beat");
 
-let response = await fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json", {method: 'GET',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  }});
-let result = await response.json();
-
-console.log(result);
+fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json")
+.then(response=> response.json())
+.then(data =>console.log(data));
