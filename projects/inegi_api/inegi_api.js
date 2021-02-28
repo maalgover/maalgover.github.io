64c37bc49e7db8a09f9f7595a2df30167526f463
maalgover <maalgover@gmail.com>
2021-03-01 00:31:28 +0100

    tweaking the fetch parameters

--- projects/inegi_api/inegi_api.js
+++ projects/inegi_api/inegi_api.js
@@ -1,3 +1,27 @@
+// fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json", {
+//   method: 'POST', // or 'PUT'
+//   headers: {
+//     'Content-Type': 'application/json',
+//     credentials: 'omit'
+//   },
+//   body: JSON.stringify(data),
+// })
+//     .then(response => response.json())
+//     .then(data =>{
+//         console.log("Success:", data);
+//     })
 
 console.log("This is just a heart beat");
 
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+      const req = new XMLHttpRequest();
+      req.open("GET","https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json",true);
+      req.send();
+      req.onload = function(){
+        const json = JSON.parse(req.responseText);
+        console.log(json)
+    }}})
+
+
+//fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json")
