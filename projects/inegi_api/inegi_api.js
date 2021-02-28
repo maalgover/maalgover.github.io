8c5fb04cf7e68025725b5601e85f8f942ba6b1c7
maalgover <maalgover@gmail.com>
2021-03-01 00:13:03 +0100

    tweaking the fetch parameters

--- projects/inegi_api/inegi_api.js
+++ projects/inegi_api/inegi_api.js
@@ -1,7 +1,14 @@
 
 
 
-fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json")
+fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json", {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+    credentials: 'omit'
+  },
+  body: JSON.stringify(data),
+})
     .then(response => response.json())
     .then(data =>{
         console.log("Success:", data);
