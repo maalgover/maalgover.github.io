febb40e1af1b330c55862b4df66bdb91c67cb557
maalgover <maalgover@gmail.com>
2021-03-01 01:09:04 +0100

    tweaking the fetch parameters

--- projects/inegi_api/inegi_api.js
+++ projects/inegi_api/inegi_api.js
@@ -1,5 +1,8 @@
 console.log("This is just a heart beat");
 
-fetch("https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json")
+let url_1="http://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/070000090002/true/BISE/2.0/f6bb8d22-b237-8aec-5d74-3326aa8f400e?type=json";
+let proxy="https://cors-anywhere.herokuapp.com/";
+fetch(url_1)
 .then(response=> response.json())
-.then(data =>console.log(data));
+.then(data =>console.log(data))
+.catch(err=>{throw err});
